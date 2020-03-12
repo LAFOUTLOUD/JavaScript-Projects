706cfdd09de6987b73eed51051233850e7fe4b26
Leon F <50891156+LAFOUTLOUD@users.noreply.github.com>
2020-03-12 17:05:54 -0400

    Update app.js

--- Budget App/app.js
+++ Budget App/app.js
@@ -1,1 +1,46 @@
-//
+var globalController = ( function() {
+		
+	// fn. to hold the code called by the EL/H
+	function ctrlAddItem() {
+			
+		// 1. Get input values
+				   
+		// 2. Add the new item to our data structure/budget controller
+				   
+		// 3. Add the new item to the UI
+				   
+		// 4. Calculate budget
+				   
+		// 5. Display the budget on the UI
+		
+		console.log('Hello World')
+			
+	}
+		   
+	// first EL/H
+	document.querySelector('.add__btn').addEventListener('click', function() {
+	
+		// calls the fn.
+		ctrlAddItem();
+	
+	})
+			
+	// second EL/H
+	document.querySelector('.add__btn').addEventListener('keypress', function(e) {
+			
+		if(e.keyCode === 13) { 
+					
+			// calls the fn.
+			ctrlAddItem();
+
+		}
+			
+	})
+			   
+	return{
+			   
+		// public code
+			   
+	}
+		   
+})();
